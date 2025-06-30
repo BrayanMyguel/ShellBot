28106c79ce36bc7dc895bec325a2e63220b48133
BrayanMyguel <accessgf05@gmail.com>
2025-06-30 22:56:33 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'BrayanMyguel‎',
   botName: "Nyx Shaytan",
   botOwner: 'Nyx Shaytan', // Your name
-  ownerNumber: '237676877404', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '237659681995', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
